70f0c4e148f6270d6e1e7e39267d8a22034b45f2
bobo-96 <boborustamov96@gmail.com>
2021-06-30 13:20:44 +0600

    add is_staf in serializer user

--- user/serializers.py
+++ user/serializers.py
@@ -72,7 +72,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'phone', 'avatar', 'first_name', 'last_name', 'email')
+        fields = ('id', 'is_staff', 'phone', 'avatar', 'first_name', 'last_name', 'email')
 
 class ShortCourseSerializer(serializers.ModelSerializer):
 
