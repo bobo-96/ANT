42222a1903928b4b9e736c629596a4e7e716d793
bobo-96 <boborustamov96@gmail.com>
2021-07-04 15:44:31 +0600

    test create method of course

--- courses/views.py
+++ courses/views.py
@@ -63,10 +63,17 @@ class CourseView(ModelViewSet):
     search_fields = ['name']
     permission_classes = (IsCourseOwnerOrReadOnly,)
 
+    # def create(self, request, *args, **kwargs):
+    #     if request.user.is_staff:
+    #         return super().create(request, *args, **kwargs)
+    #     return Response('нет доступа', status=status.HTTP_403_FORBIDDEN)
+
     def create(self, request, *args, **kwargs):
-        if request.user.is_staff:
-            return super().create(request, *args, **kwargs)
-        return Response('нет доступа', status=status.HTTP_403_FORBIDDEN)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
     def get_object(self):
